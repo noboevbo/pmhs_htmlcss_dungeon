7a75131fa3c95e187ad69d2051000b41f956aed0
Dennis Burgermeister <dennis@dennisnotes.com>
2022-03-19 11:39:32 +0100

    (c) updates

--- js/exercises/00_tutorial.js
+++ js/exercises/00_tutorial.js
@@ -15,7 +15,11 @@ let validationFuncs = [
     }
 ]
 
-let instructions = `<ol><li>Im Feld <em>Auftrag</em> stehen immer die Arbeitsaufträge</li><li>Lies dir den Text im Feld <em>Ergebnis</em> durch. Dort wird die HTML Datei aus dem ordner <em>Aufgaben</em> so angezeigt, wie sie bearbeitet wurde. In diesem Tutorial findest du dort noch einige Infos.</li></ol>`
+let instructions = `
+<ol>
+    <li>Im Feld <em>Auftrag</em> stehen immer die Arbeitsaufträge</li>
+    <li>Lies dir den Text im Feld <em>Ergebnis</em> durch. Dort wird die HTML Datei aus dem ordner <em>Aufgaben</em> so angezeigt, wie sie bearbeitet wurde. In diesem Tutorial findest du dort noch einige Infos.</li>
+</ol>`
 
 let tips = [{
     level: 0,
