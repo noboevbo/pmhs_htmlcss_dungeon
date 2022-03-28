a1931ac2654bb78d8abf87033c9b6e0183214288
Dennis Burgermeister <dennis@dennisnotes.com>
2022-03-28 19:06:54 +0200

    (c) updates

--- js/exercises/13_css_navbar.js
+++ js/exercises/13_css_navbar.js
@@ -1,4 +1,4 @@
-import { classCheckStyleSameValue, classCheckStyleValues, classHasCorrectStyleValue, cssBorderColorNames, cssBorderStyleNames, cssBorderWidthNames, cssMarginNames, cssPaddingNames} from '../exercise/validation_helper.js';
+import { classCheckStyleSameValue, classCheckStyleValues, classHasCorrectStyleValue, cssBorderColorNames, cssBorderStyleNames, cssBorderWidthNames, cssMarginNames, cssPaddingNames, hasClassStyleValue as hasStyleValue, hasQuerySelectorCorrectStyleValue} from '../exercise/validation_helper.js';
 import { Exercise } from '../exercise/exercise_base.js';
 
 let exerciseID = "13_css_navbar";
@@ -11,9 +11,8 @@ let instructions = `
 <li>Setze die Farbe von Links in jedem ListItem auf weiß. Nutze dafür verschachtelte Selektoren!</li>
 <li>Füge einen Hover Effekt für jeden Link in jedem ListItem ein, bei dem die Hintergrundfarbe auf weiß und die Textfarbe auf schwarz gesetzt wird.</li>
 <li>Die Liste ist noch zu breit, lege eine fixe Breite von 200px für die ungeordnete Liste fest</li>
-<li>Setze außerdem den Innen- und Außenabstand für alle Seiten der ungeordneten Liste auf 0.</li>
+<li>Entferne den Unterstrich der Links!</li>
 <li>Die Links in allen Listenelementen sollen nun die gesamte verfügbare Breite nutzen. Ändere dafür deren Anzeigeart von <em>inline</em> zu <em>block</em>.
-<li>Füge noch einen Innenabstand von <em>8px 16px</em> für alle Links in allen ListenElementen ein.</li>
 </ol>
 `
 
@@ -22,25 +21,22 @@ let tips = [
   {level: 1, title: "Benötigte HTML Elemente", content: "Den Hintergrund ändert man mit der Eigenschaft <em>background-color</em> und die Textfarbe mit der Eigenschaft <em>color</em>. Sieh dir den Link zu den Farbnamen an um Informationen über vordefinierte Farben zu erhalten.", weblinks: ["https://wiki.selfhtml.org/wiki/CSS/Tutorials/Hintergrund/Gestaltung_mit_CSS", "https://www.w3schools.com/colors/colors_names.asp"]},
   {level: 3, title: "Lösung anzeigen", content: `Die Lösung ist: <xmp>
   <style id="meinStyle">
-    ul {
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
-      width: 200px;
-      background-color: #000;
-    }
+  ul {
+    list-style-type: none;
+    width: 200px;
+    background-color: #000;
+  }
 
-    li a {
-      display: block;
-      color: #fff;
-      padding: 8px 16px; 
-      text-decoration: none;
-    }
+  li a {
+    display: block;
+    color: #fff;
+    text-decoration: none;
+  }
 
-    li a:hover {
-      background-color: #fff;
-      color: #000;
-    }
+  li a:hover {
+    background-color: #fff;
+    color: #000;
+  }
   </style>
 [...]
   <!-- Änderungen im Body ab hier einfügen -->
@@ -54,14 +50,14 @@ let tips = [
 ]
 
 let validationFuncs = [
-  function() { return classHasCorrectStyleValue("box", "background-color", "rgb(255, 0, 0)"); },
-  function() { return classHasCorrectStyleValue("box", "width", "200px"); },
-  function() { return classHasCorrectStyleValue("box", "height", "200px"); },
-  function() { return classCheckStyleSameValue("box", cssBorderColorNames, "rgb(0, 0, 255)"); },
-  function() { return classCheckStyleSameValue("box", cssBorderWidthNames, "20px"); },
-  function() { return classCheckStyleSameValue("box", cssBorderStyleNames, "solid"); },
-  function() { return classCheckStyleSameValue("box", cssPaddingNames, "20px"); },
-  function() { return classCheckStyleValues("box", cssMarginNames, ["15px", "30px", "15px", "30px"]); },
+  function() { return hasQuerySelectorCorrectStyleValue("ul", "list-style-type", `none`); },
+  function() { return hasQuerySelectorCorrectStyleValue("ul", "background-color", "rgb(0, 0, 0)"); },
+  function() { return hasQuerySelectorCorrectStyleValue("li a", "color", "rgb(255, 255, 255)"); },
+  function() { return hasStyleValue("li a:hover", "color", "rgb(0, 0, 0)"); },
+  function() { return hasStyleValue("li a:hover", "background-color", "rgb(255, 255, 255)"); },
+  function() { return hasQuerySelectorCorrectStyleValue("ul", "width", `200px`); },
+  function() { return hasStyleValue("li a", "text-decoration", "none"); },
+  function() { return hasQuerySelectorCorrectStyleValue("li a", "display", "block"); },
 ]
 
 let exerciseBase = new Exercise(exerciseID, instructions, tips, validationFuncs);
